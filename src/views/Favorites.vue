a5d66996e2adedc0bd34c023d57e4ee782f9fcda
essasabbagh <essasabbagh@gmail.com>
2021-02-22 01:08:27 +0300

    fix

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="favorites">
     <h1 class="mb-5 mt-5">Favorites Movies</h1>
-    <p v-if="favList.length == 0">There is no Movies</p>
-
+    <!-- <p v-if="favList.length == 0">There is no Movies</p> -->
+    <p v-if="favList.length == 0" class="mx-auto fs-1 text-muted">
+      There is no movie <span class="fs-2 text-muted">ㄟ( ▔, ▔ )ㄏ</span>
+    </p>
     <Movies
       v-for="(movie, index) in favList"
       :key="index"
@@ -19,39 +21,39 @@ import axios from "axios";
 import Movies from "@/components/Movies";
 export default {
   components: {
-    Movies
+    Movies,
   },
   data() {
     return {
-      favList: []
+      favList: [],
     };
   },
   methods: {
     removeFromFavorite(item) {
       axios
         .delete(`http://localhost:3000/myFav/${item.id}`)
-        .then(res => {
+        .then((res) => {
           console.log(res);
-          this.favList = this.favList.filter(e => e.id !== item.id) || [];
+          this.favList = this.favList.filter((e) => e.id !== item.id) || [];
           console.log("FavoriteList", this.favList);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         });
-    }
+    },
   },
   created() {
     axios
       .get(`http://localhost:3000/myFav`)
-      .then(movie_fav_response => {
+      .then((movie_fav_response) => {
         console.log("movie_fav_response", movie_fav_response.data);
         this.favList = movie_fav_response.data || [];
         console.log("this.favList", this.favList);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-  }
+  },
 };
 </script>
 <style>
