ff311d450a011189bc2cc99b73a690a689a5d1e0
essasabbagh <essasabbagh@gmail.com>
2021-02-23 09:24:31 +0300

    about setup

---
+++ src/views/About.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="about container mt-5">
+    <h1 class="">About</h1>
+    <p>
+      This application made for assignment in Vue js
+      <a
+        href="https://www.kodluyoruz.org/bootcamp/istanbul-uygulamali-vue-js-bootcamp/"
+        target="_blank"
+        >Kodluyoruz</a
+      >
+      bootcamp
+    </p>
+    <ul>
+      <li><strong>Html & Css: </strong>For structure a web page</li>
+      <li>
+        <strong
+          ><a href="https://v3.vuejs.org/" target="_blank">Vuejs 3</a>
+          : </strong
+        >including vue-router
+      </li>
+      <li>
+        <strong
+          ><a href="https://getbootstrap.com/" target="_blank">Bootstrap 5</a>
+          : </strong
+        >For styling
+      </li>
+      <li>
+        <strong
+          ><a target="”_blank”" href="http://www.omdbapi.com/"
+            >OMDb API:
+          </a></strong
+        >For movies database
+      </li>
+      <li>
+        <strong
+          ><a target="”_blank”" href="https://fonts.google.com/specimen/Poppins"
+            >Poppins font</a
+          >
+          :</strong
+        >From google fonts
+      </li>
+      <li>
+        <strong
+          ><a href="https://firebase.google.com/" target="_blank">Firebase</a
+          >: </strong
+        >For hosting
+      </li>
+    </ul>
+    <div>
+      <h6 class="text-center">Links</h6>
+      <div class="fs-2">
+        <a
+          class="m-5"
+          href="https://github.com/essasabbagh/Movie-App"
+          target="_blank"
+          ><i class="fab fa-github"></i
+        ></a>
+        <a href="https://twitter.com/3ssasabbagh" target="_blank"
+          ><i class="fab fa-twitter"></i
+        ></a>
+        <a class="m-5" href="http://" target="_blank"
+          ><i class="fas fa-envelope"></i
+        ></a>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.about {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: column;
+}
+li {
+  margin-top: 5px;
+}
+a {
+  color: #ff3737;
+}
+.footer {
+  display: flex;
+  flex-direction: column;
+}
+</style>
