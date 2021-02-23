8d0ae7e09bc31df9b3c181e7b0e1efef5aa0068b
essasabbagh <essasabbagh@gmail.com>
2021-02-23 14:36:43 +0300

    favorit rebuild

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="favorites">
     <h1 class="mb-5 mt-5">Favorites Movies</h1>
-    <p v-if="favList.length == 0" class="mx-auto fs-1 text-muted">
-      There is no movie <span class="fs-2 text-muted">ㄟ( ▔, ▔ )ㄏ</span>
-    </p>
     <div class="container">
       <Movies
         v-for="(movie, index) in favList"
         :key="index"
         :details="movie"
-        :favList="favList"
-        @Remove-From-Favourite-event="removeFromFavorite"
         @click="$router.push(`/details/${movie.imdbID}`)"
       />
+      <div v-if="favList.length == 0" class="mx-auto text-center">
+        <p class="fs-1 text-muted">There is no movie</p>
+        <p><span class="fs-1 text-muted">ㄟ( ▔, ▔ )ㄏ</span></p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import Movies from "@/components/Movies";
 export default {
   components: {
@@ -30,30 +28,33 @@ export default {
     };
   },
   methods: {
-    removeFromFavorite(item) {
-      axios
-        .delete(`http://localhost:3000/myFav/${item.id}`)
-        .then((res) => {
-          console.log(res);
-          this.favList = this.favList.filter((e) => e.id !== item.id) || [];
-          console.log("FavoriteList", this.favList);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
+    // removeFromFavorite(item) {
+    //   axios
+    //     .delete(`http://localhost:3000/myFav/${item.id}`)
+    //     .then((res) => {
+    //       console.log(res);
+    //       this.favList = this.favList.filter((e) => e.id !== item.id) || [];
+    //       console.log("FavoriteList", this.favList);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+    // },
   },
   created() {
-    axios
-      .get(`http://localhost:3000/myFav`)
-      .then((movie_fav_response) => {
-        console.log("movie_fav_response", movie_fav_response.data);
-        this.favList = movie_fav_response.data || [];
-        console.log("this.favList", this.favList);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    this.favList = JSON.parse(localStorage.getItem("movie")) || [];
+    console.log("fav",this.favList);
+
+    // axios
+    //   .get(`http://localhost:3000/myFav`)
+    //   .then((movie_fav_response) => {
+    //     console.log("movie_fav_response", movie_fav_response.data);
+    //     this.favList = movie_fav_response.data || [];
+    //     console.log("this.favList", this.favList);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   },
 };
 </script>
