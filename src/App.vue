40527b74120e6acbfcfa4f7c3ee8968df4b875c2
essasabbagh <essasabbagh@gmail.com>
2021-03-23 11:54:16 +0300

    clean style

--- src/App.vue
+++ src/App.vue
@@ -38,28 +38,14 @@ export default {
 }
 
 .moveUp-enter-active {
-  animation: fadeIn 0.5s ease;
+  animation: fadeIn 0.3s ease;
 }
 @keyframes fadeIn {
   0% {
     opacity: 0;
   }
-  50% {
-    opacity: 0.5;
-  }
   100% {
     opacity: 1;
-  }
-}
-.moveUp-leave-active {
-  animation: moveUp 0.3s ease;
-}
-@keyframes moveUp {
-  0% {
-    transform: translateY(0);
-  }
-  100% {
-    transform: translateY(-400px);
   }
 }
 </style>
