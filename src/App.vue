1347125266afdfbfded2ffac41da48bfc07056d1
essasabbagh <essasabbagh@gmail.com>
2021-03-24 11:16:31 +0300

    add router animation

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
     <!-- <transition name="moveUp" mode="out-in">
       <router-view />
     </transition> -->
-    
+
     <router-view v-slot="{ Component }">
-    <transition name="route" mode="out-in">
-      <component :is="Component"></component>
-    </transition>
-  </router-view>
+      <transition name="route" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
     <ScrollTopComponent />
   </div>
 </template>
@@ -29,6 +29,7 @@ export default {
 
 <style>
 #nav {
+
   font-size: larger;
   width: 100%;
   box-shadow: 2px 2px 10px rgba(34, 34, 34, 0.5);
@@ -55,20 +56,19 @@ export default {
   }
 }
 
-
 /* route transitions */
 .route-enter-from {
   opacity: 0;
   transform: translateX(100px);
 }
 .route-enter-active {
-  transition: all 0.3s ease-out; 
+  transition: all 0.3s ease-out;
 }
 .route-leave-to {
   opacity: 0;
   transform: translateX(-100px);
 }
 .route-leave-active {
-  transition: all 0.3s ease-in; 
+  transition: all 0.3s ease-in;
 }
 </style>
